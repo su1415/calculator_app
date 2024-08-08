d2311768e8938a753582b5335d4f6e1c1d850461
su1415 <suyams@gmail.com>
2024-08-08 21:30:21 +0900

    feat: 見た目を作成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const buttons = [
+    ["C", "", "", "/"],
+    ["7", "8", "9", "*"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["", "0", "", "="],
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="display">
+        { 0 }
+      </div>
+      <div className="button-panel">
+        { buttons.map((row, rowIndex) => (
+          <div key={ rowIndex } className="button-row">
+            { row.map((buttonName, colIndex) => (
+              <button key={ rowIndex * row.length + colIndex }>
+                { buttonName }
+              </button>
+            )) }
+          </div>
+        )) }
+      </div>
     </div>
   );
 }
