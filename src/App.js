ae113e4a1e5a8df665e5233f3d756014ae0b4d76
su1415 <suyams@gmail.com>
2024-08-08 22:12:20 +0900

    refactor: ButtonPanelを分離

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React from "react";
 import Display from "./components/Display";
+import ButtonPanel from "./components/ButtonPanel";
 import "./App.css";
 
 function App() {
-  const buttons = [
-    ["C", "", "", "/"],
-    ["7", "8", "9", "*"],
-    ["4", "5", "6", "-"],
-    ["1", "2", "3", "+"],
-    ["", "0", "", "="],
-  ];
-
   return (
     <div className="App">
       <Display value={ 0 } />
-      <div className="button-panel">
-        { buttons.map((row, rowIndex) => (
-          <div key={ rowIndex } className="button-row">
-            { row.map((buttonName, colIndex) => (
-              <button key={ rowIndex * row.length + colIndex }>
-                { buttonName }
-              </button>
-            )) }
-          </div>
-        )) }
-      </div>
+      <ButtonPanel />
     </div>
   );
 }
---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function ButtonPanel() {
+  const buttons = [
+    ["C", "", "", "/"],
+    ["7", "8", "9", "*"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["", "0", "", "="],
+  ];
+
+  return (
+    <div className="button-panel">
+      { buttons.map((row, rowIndex) => (
+        <div key={ rowIndex } className="button-row">
+          { row.map((buttonName, colIndex) => (
+            <button key={ rowIndex * row.length + colIndex }>
+              { buttonName }
+            </button>
+          )) }
+        </div>
+      )) }
+    </div>
+  );
+}
+
+export default ButtonPanel;
